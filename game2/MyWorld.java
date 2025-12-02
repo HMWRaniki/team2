d227b40c4644dd5252667ced866dfcbaf84e45dc
Taiki21079 <21079@hakodate.kosen-ac.jp>
2025-12-02 09:22:46 +0900

    背景の表示

--- game2/MyWorld.java
+++ game2/MyWorld.java
@@ -16,6 +16,10 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1500, 950, 1);
+        
+        addObject( new goal1(), 67, 493 );
+        addObject( new goal2(), 1413, 493 );
+        addObject( new waku(), 740, 490 );
     }
 }
